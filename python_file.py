a54d1b7857e2595eab30f4305b64cd7a8617bc95
chitti-hue <chittibabu.sidda@gmail.com>
2025-02-02 12:34:43 +0530

    initial commmit

---
+++ python_file.py
@@ -0,0 +1,14 @@
+# Function to add two numbers
+def add_numbers(a, b):
+    return a + b
+
+
+# Main part of the program
+if __name__ == "__main__":
+    # Using the add_numbers function
+    num1 = 5
+    num2 = 3
+    result = add_numbers(num1, num2)
+    print(f"The sum of {num1} and {num2} is {result}.")
+
+   
